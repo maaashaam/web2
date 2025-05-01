be8f37907f85e30e4a6f8f578b8c7286a00dd140
Maria Mashkovtseva <marie.mashkovtseva@yandex.ru>
2025-05-01 13:27:28 +0300

    Add styles

--- src/main.js
+++ src/main.js
@@ -7,11 +7,10 @@ const dogsss = 'live_bKhR2sj3sl07nONV4MWU5meXp8mFSqzHmwFliaYQJt2QiAXcs0bLDhIt080
 async function fetchRandom() {
   const res = await fetch(`https://api.thedogapi.com/v1/images/search?api_key=${dogsss}`);
   const data = await res.json();
-  console.log(data[0].url);
   return data[0].url; 
 }
 
-async function loadImage(count = 5) {
+async function loadImage(count = 8) {
   dogList.innerHTML = '';
   const promises = Array.from({ length: count }, () => fetchRandom());
   const dogs = await Promise.all(promises);
@@ -20,8 +19,6 @@ async function loadImage(count = 5) {
     const li = document.createElement('li');
     const img = document.createElement('img');
     img.src = dogUrl;
-    img.style.width = '100px';
-    img.style.cursor = 'pointer';
     img.addEventListener('click', () => showDogDetail(dogUrl));
     li.appendChild(img);
     dogList.appendChild(li);
